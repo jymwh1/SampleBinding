135f9411108e94bf11b9755091b146f902bebf00
jymwh1 <jymwh1@users.noreply.github.com>
2021-03-03 15:48:05 +0800

    添加项目文件。

---
+++ SampleBinding/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleBinding
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public RegisterAndValue Register_D12102 = new RegisterAndValue("D12102", 1);
+        public CheckBoxBaseic cb_BasicBind = new CheckBoxBaseic();
+
+        public RegisterAndValue0 Register_L40 = new RegisterAndValue0("L4");
+        public RegisterAndValue Register_L4 = new RegisterAndValue("L4");
+        public RegisterAndValue1 Register_L41 = new RegisterAndValue1("L4");
+        public RegisterAndValueB Register_X100 = new RegisterAndValueB();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbInputNumber.SetBinding(TextBox.TextProperty, new Binding("sValue") { Source = Register_D12102, Mode = BindingMode.TwoWay });
+            
+            cb_Basic.DataContext = cb_BasicBind;
+
+            MCB_Origin_L40.DataContext = Register_L40.IsintCheck;//Register_L40和Register_L4本质一样，只是想说明xaml中绑定对象为变量的问题
+            MCB_Origin_L4.DataContext = Register_L4.aci;//只有aci里面有IsintCheck属性
+            MCB_Origin_L41.DataContext = Register_L41;
+
+            rbt_Input_X100.DataContext = Register_X100.IsCheck;
+        }
+
+       
+
+        private void Bt_Jog_M3_Click(object sender, RoutedEventArgs e)
+        {
+            Register_D12102.sValue = "Hello";
+            cb_BasicBind.i_Check = 1;
+
+            Register_L40.IsintCheck.IsintCheck = 9;//其实本质上这个checkbox的IsCheck已经只是用来和字典关联的了<Trigger Property="CheckBox.IsChecked" Value="true">
+            Register_L41.IsintCheck = 10;
+            Register_L4.aci.IsintCheck = 11;
+
+            Register_X100.IsCheck.IsCheck = true;
+        }
+
+        private void Bt_Jog_M4_Click(object sender, RoutedEventArgs e)
+        {
+            Register_D12102.sValue = "bye~bye~";
+            cb_BasicBind.i_Check = 0;
+
+
+            Register_L40.IsintCheck.IsintCheck = 0;
+            Register_L41.IsintCheck = 0;
+            Register_L4.aci.IsintCheck = 0;
+
+            Register_X100.IsCheck.IsCheck= false;
+        }
+    }
+}
---
+++ SampleBinding/PLCClass.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBinding
+{
+    /// <summary>
+    /// 以后统一用这个方法绑定，大大节省代码量cnblogs.com/zh7791/p/9933954.html
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //tbInputNumber.SetBinding(TextBox.TextProperty, new Binding("sValue") { Source = Register_D12102, Mode = BindingMode.TwoWay });
+        //cb_Basic.DataContext = cb_BasicBind;
+        //上面语句分别为对应对象的为PropertyChanged添加了 += 不然它为空
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    
+
+    public class CheckBoxBaseic : ViewModelBase
+    {
+        public CheckBoxBaseic()
+        {
+            _i_Check = 0;
+        }
+
+        private int _i_Check;
+
+        public int i_Check
+        {
+            get { return _i_Check; }
+            set
+            {
+                _i_Check = value;
+                OnPropertyChanged();
+            }
+        }      
+
+    }
+
+    /// <summary>
+    /// 用来绑定界面textbox的值
+    /// </summary>
+    public class RegisterAndValue : ViewModelBase
+    {
+        // 构造函数
+        public RegisterAndValue(string sregister)
+        {
+            t_isintCheck = new IsintCheckChange();
+            sRegister = sregister;
+            sValue = string.Empty;
+            ecount = 1;
+        }
+
+        public RegisterAndValue(string sregister, int icount)
+        {
+            t_isintCheck = new IsintCheckChange();
+            sRegister = sregister;
+            sValue = string.Empty;
+            ecount = icount;
+        }
+
+        public int ecount;  // 寄存器个数
+
+        private string t_register = "";
+
+        public string sRegister
+        {
+            get { return this.t_register; }
+            set { this.t_register = value; }
+        }
+
+        public string t_value = "";
+        public string sValue
+        {
+            get { return t_value; }
+            set
+            {
+                t_value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IsintCheckChange t_isintCheck;
+        public IsintCheckChange aci
+        {
+            get
+            {
+                return t_isintCheck;
+            }
+            set
+            {
+                t_isintCheck = value;
+            }
+        }
+    }
+
+    public class IsintCheckChange : ViewModelBase
+    {
+        public IsintCheckChange()
+        {
+            IsintCheck = 0;
+        }
+        private int _isintcheck;
+        public int IsintCheck
+        {
+            get
+            {
+                return _isintcheck;
+            }
+            set
+            {
+                _isintcheck = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class RegisterAndValue0 : ViewModelBase
+    {
+        // 构造函数
+        public RegisterAndValue0(string sregister)
+        {
+            t_isintCheck = new IsintCheckChange();
+            sRegister = sregister;
+            sValue = string.Empty;
+            ecount = 1;
+        }
+
+        public RegisterAndValue0(string sregister, int icount)
+        {
+            t_isintCheck = new IsintCheckChange();
+            sRegister = sregister;
+            sValue = string.Empty;
+            ecount = icount;
+        }
+
+        public int ecount;  // 寄存器个数
+
+        private string t_register = "";
+
+        public string sRegister
+        {
+            get { return this.t_register; }
+            set { this.t_register = value; }
+        }
+
+        public string t_value = "";
+        public string sValue
+        {
+            get { return t_value; }
+            set
+            {
+                t_value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IsintCheckChange t_isintCheck;
+        public IsintCheckChange IsintCheck
+        {
+            get
+            {
+                return t_isintCheck;
+            }
+            set
+            {
+                t_isintCheck = value;
+            }
+        }        
+    }
+
+    public class RegisterAndValue1 : ViewModelBase
+    {
+        public RegisterAndValue1(string sregister)
+        {
+            _isintcheck = 0;
+            sRegister = sregister;
+            sValue = string.Empty;
+            ecount = 1;
+        }
+
+        public int ecount;  // 寄存器个数
+
+        private string t_register = "";
+
+        public string sRegister
+        {
+            get { return this.t_register; }
+            set { this.t_register = value; }
+        }
+
+        public string t_value = "";
+        public string sValue
+        {
+            get { return t_value; }
+            set
+            {
+                t_value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _isintcheck;
+        public int IsintCheck
+        {
+            get
+            {
+                return _isintcheck;
+            }
+            set
+            {
+                _isintcheck = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+
+    public class RegisterAndValueB
+    {
+        public RegisterAndValueB()
+        {
+            _IsCheck = new IsInCheck();
+        }
+
+        private string _register;
+        public string register
+        {
+            get { return _register; }
+            set { _register = value; }
+        }
+
+
+        private IsInCheck _IsCheck;
+        public IsInCheck IsCheck
+        {
+            get { return _IsCheck; }
+            set { _IsCheck = value; }
+        }
+
+    }
+
+    public class IsInCheck:ViewModelBase
+    {
+        private bool _IsCheck;
+        public bool IsCheck
+        {
+            get { return _IsCheck; }
+            set
+            {
+                _IsCheck = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
